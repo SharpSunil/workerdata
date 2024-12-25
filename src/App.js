3d34bedae12c1e51b5019a5123e4dc35eb660704
Sunil Shelke <shelkesunil072@gmail.com>
2024-12-25 16:07:12 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Workertable from './componant/Workertable';
+
+function App() {
+  return (
+    <div className="App">
+     <Workertable />
+    </div>
+  );
+}
+
+export default App;
